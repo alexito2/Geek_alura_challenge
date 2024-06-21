23b6860b353398aeed58f02380ac6699669384c8
Alejandro Garcia <85403880+alexito2@users.noreply.github.com>
2024-06-21 13:55:04 -0600

    Update printProducts.js

--- js/controller/printProducts.js
+++ js/controller/printProducts.js
@@ -3,7 +3,7 @@ import { deleteProduct } from "./deletProducts.js";
 function printProducts() {
     let products = document.getElementById('products');
 
-    fetch("http://localhost/products")
+    fetch("http://localhost:3000/products")
         .then(response => response.json())
         .then(data => {
             data.forEach(element => {
